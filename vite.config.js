4ce5530504d573c0825cd75af80fb975514e3de5
Keny R C Noleto <kenyrcnoleto@gmail.com>
2025-05-15 19:09:44 -0300

    crm-1: instaling tailwing css

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
+import forms from '@tailwindcss/forms'
+import typo from '@tailwindcss/typography'
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
+        tailwindcss(), forms, typo,
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
