0dd74a737b417208eff28b2b72dfc26e95004ca4
Keny R C Noleto <kenyrcnoleto@gmail.com>
2025-05-16 11:15:56 -0300

    crm-1: installing mary-ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,19 @@ import  typo from '@tailwindcss/typography'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-     "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-    "./app/Livewire/**/*.php",
-  ],
+		"./resources/**/*.blade.php",
+		 "./resources/**/*.js",
+		 "./resources/**/*.vue",
+		 "./app/Livewire/**/*.php",
+		 "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
+	],
   theme: {
     extend: {},
   },
   plugins: [
-    forms, typo
-  ],
+		forms,
+		typo,
+		require("daisyui")
+	],
 }
 
