46efb2608b8194330dc7f701c85c4a0f6b2c01ed
Keny R C Noleto <kenyrcnoleto@gmail.com>
2025-05-19 10:40:51 -0300

    crm-1: bug resolution in file vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
-import forms from '@tailwindcss/forms'
-import typo from '@tailwindcss/typography'
-
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
-        tailwindcss(), forms, typo,
+
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
