32cb3f380e0c95bcccbfdcca86e79b764626a507
Keny R C Noleto <kenyrcnoleto@gmail.com>
2025-05-20 14:22:40 -0300

    crm-1: email needs to be unique

--- tests/Feature/Livewire/Auth/RegisterTest.php
+++ tests/Feature/Livewire/Auth/RegisterTest.php
@@ -48,10 +48,18 @@ test('shoulb be able to register a new user in the system', function () {
 
 test('validation rules', function ($f) {
     // dd($f);
-    Livewire::test(Register::class)
-        ->set($f->field, $f->value)
-        ->call('submit')
-        ->assertHasErrors([$f->field => $f->rule]);
+    if ($f->rule == 'unique') {
+        User::factory()->create([$f->field => $f->value]);
+    }
+    $livewire = Livewire::test(Register::class)
+        ->set($f->field, $f->value);
+
+    if (property_exists($f, 'aValue')) {
+        $livewire->set($f->aField, $f->aValue);
+    }
+
+    $livewire->call('submit')
+    ->assertHasErrors([$f->field => $f->rule]);
 
 })->with([
     'name::required'     => (object)['field' => 'name', 'value' => '', 'rule' => 'required'],
@@ -60,5 +68,6 @@ test('validation rules', function ($f) {
     'email::email'       => (object)['field' => 'email', 'value' => 'not-an-email', 'rule' => 'email'],
     'email::max:255'     => (object)['field' => 'email', 'value' => str_repeat('*' . '@doe.com', 256), 'rule' => 'max'],
     'email::confirmed'   => (object)['field' => 'email', 'value' => 'joe@doe.com', 'rule' => 'confirmed'],
+    'email::unique'      => (object)['field' => 'email', 'value' => 'joe@doe.com', 'rule' => 'unique' , 'aField' => 'email_confirmation', 'aValue' => 'joe@doe.com'],
     'password::required' => (object)['field' => 'password', 'value' => '', 'rule' => 'required'],
 ]);
