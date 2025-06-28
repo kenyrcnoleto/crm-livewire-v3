12f9ab0320383000b302956b2db94bdab8f89b8c
Keny R C Noleto <kenyrcnoleto@gmail.com>
2025-06-28 13:02:03 -0300

    crm-5: create a middleware to protect admin routes

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,23 @@ use Illuminate\Support\Facades\{Auth, Route};
 |
 */
 
+//region Login Flow
 Route::get('/login', Login::class)->name('login');
 Route::get('/register', Register::class)->name('auth.register');
 Route::get('/logout', fn () => Auth::logout());
 Route::get('/password/recovery', Password\Recovery::class)->name('password.recovery');
 Route::get('/password/reset', Password\Reset::class)->name('password.reset');
+//endregion
 
+//region Authnticated
 Route::middleware('auth')->group(function () {
 
     Route::get('/', Welcome::class)->name('dashboard');
 
+    //region Admin
+    Route::prefix('/admin')->middleware('can:be-an-admin')->group(function () {
+        Route::get('/dashboard', fn () => 'admin.dashboard')->name('admin.dashboard');
+    });
+    //endregion
 });
+//endregion
