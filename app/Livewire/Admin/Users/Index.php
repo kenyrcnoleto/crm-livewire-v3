1d860d498396f24a43ad5a3a062aa0ee2e8820a1
Keny R C Noleto <kenyrcnoleto@gmail.com>
2025-07-09 12:40:06 -0300

    crm-12: check the table format

--- app/Livewire/Admin/Users/Index.php
+++ app/Livewire/Admin/Users/Index.php
@@ -2,23 +2,44 @@
 
 namespace App\Livewire\Admin\Users;
 
+use App\Enum\Can;
 use App\Models\User;
 use Illuminate\Contracts\View\View;
-use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Collection;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Index extends Component
 {
+    /**
+     * @property-read Collection|User[] $users
+     */
+    public function mount()
+    {
+        $this->authorize(Can::BE_AN_ADMIN->value);
+    }
+
     public function render(): View
     {
         return view('livewire.admin.users.index');
     }
 
     #[Computed()]
-    public function users(): LengthAwarePaginator
+    public function users(): Collection
     {
         // dd();
-        return User::paginate();
+        return User::all();
+    }
+
+    #[Computed]
+    public function headers(): array
+    {
+        return [
+            ['key' => 'id', 'label' => '#'],
+            ['key' => 'name', 'label' => 'Name'],
+            ['key' => 'email', 'label' => 'Email'],
+            ['key' => 'permissions', 'label' => 'Permissions '],
+        ];
+
     }
 }
--- tests/Feature/Admin/UserMagement/ListUsersTest.php
+++ tests/Feature/Admin/UserMagement/ListUsersTest.php
@@ -2,7 +2,6 @@
 
 use App\Livewire\Admin;
 use App\Models\User;
-use Illuminate\Pagination\LengthAwarePaginator;
 use Livewire\Livewire;
 
 use function Pest\Laravel\{actingAs, get};
@@ -28,14 +27,15 @@ test('making sure that the route is protecd by the permission BE_AN_ADMIN', func
 });
 
 test("let's create a livewire component to list all users in the page", function () {
-
+    actingAs(
+        User::factory()->admin()->create()
+    );
     $users = User::factory()->count(10)->create();
 
     $lw = Livewire::test(Admin\Users\Index::class);
     $lw->assertSet('users', function ($users) {
         expect($users)
-            ->toBeInstanceOf(LengthAwarePaginator::class)
-            ->toHaveCount(10);
+            ->toHaveCount(11);
 
         return true;
     });
@@ -43,4 +43,17 @@ test("let's create a livewire component to list all users in the page", function
     foreach ($users as $user) {
         $lw->assertSee($user->name);
     }
+});
+
+test('check the table format', function () {
+    actingAs(
+        User::factory()->admin()->create()
+    );
+    Livewire::test(Admin\Users\Index::class)
+        ->assertSet('headers', [
+            ['key' => 'id', 'label' => '#'],
+            ['key' => 'name', 'label' => 'Name'],
+            ['key' => 'email', 'label' => 'Email'],
+            ['key' => 'permissions', 'label' => 'Permissions '],
+        ]);
 });
