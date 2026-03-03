a2a4fdf122e56ce3792033ad12e85b15563ddec6
Keny R C Noleto <kenyrcnoleto@gmail.com>
2026-03-03 11:30:06 -0300

    crm-14: backend: confirming before deletion

--- tests/Feature/Admin/UserMagement/DeleteUserTest.php
+++ tests/Feature/Admin/UserMagement/DeleteUserTest.php
@@ -4,7 +4,7 @@ use App\Livewire\Admin;
 use App\Models\User;
 use Livewire\Livewire;
 
-use function Pest\Laravel\{actingAs, assertSoftDeleted};
+use function Pest\Laravel\{actingAs, assertNotSoftDeleted, assertSoftDeleted};
 
 test('it should be ablet to delete a user', function () {
     $user        = User::factory()->admin()->create();
@@ -13,10 +13,27 @@ test('it should be ablet to delete a user', function () {
     actingAs($user);
 
     Livewire::test(Admin\Users\Delete::class, ['user' => $forDeletion])
+        ->set('confirmation_confirmation', 'kenobi')
         ->call('destroy')
         ->assertDispatched('user::deleted');
 
     assertSoftDeleted('users', [
+        'id' => $forDeletion->id,
+    ]);
+})->only();
+
+test('it should have a confirmation before deletion', function () {
+    $user        = User::factory()->admin()->create();
+    $forDeletion = User::factory()->create();
+
+    actingAs($user);
+
+    Livewire::test(Admin\Users\Delete::class, ['user' => $forDeletion])
+        ->call('destroy')
+        ->assertHasErrors(['confirmation' => 'confirmed'])
+        ->assertNotDispatched('user::deleted');
+
+    assertNotSoftDeleted('users', [
         'id' => $forDeletion->id,
     ]);
 })->only();
