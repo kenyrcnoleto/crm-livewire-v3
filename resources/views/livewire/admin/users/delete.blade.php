61e1a440559da3cd2c0cd06db565264d3d64d941
Keny R C Noleto <kenyrcnoleto@gmail.com>
2026-03-10 18:00:49 -0300

    crm-14: frontend: creation of modal for delete action

--- resources/views/livewire/admin/users/delete.blade.php
+++ resources/views/livewire/admin/users/delete.blade.php
@@ -1,4 +1,26 @@
 <div>
     {{-- Do your work, then step back. --}}
-    <button wire:click="$set('modal', true)" class="btn btn-danger">Delete</button>
+    <x-button icon="o-trash" @click="$wire.modal = true" spinner class="btn-sm" />
+
+    <x-modal wire:model="modal" title="Deletion Confirmation" separator>
+
+        @error('confirmation')
+            <x-alert icon="o-exclamation-triangle" class="alert-danger"  >
+            {{ $message }}
+            </x-alert>
+        @enderror
+
+        <x-input label="Write 'KENOBI' to confirm the deletion of the user {{ $user->name }}"
+             wire:model="confirmation_confirmation" />
+
+         {{-- We can use the `actions` slot from `x-modal` to place the buttons in the right place, but we can also place them outside of it, as long as we use the `actions` slot from `x-form` inside the modal. --}}
+         {{-- <x-slot:actions></x-slot:actions>
+            <x-button label="Cancel" @click="$wire.modal = false" />
+
+        {{-- Notice we are using now the `actions` slot from `x-form`, not from modal --}}
+        <x-slot:actions>
+            <x-button label="Cancel" @click="$wire.modal = false" />
+            <x-button label="Confirm" class="btn-primary" wire:click="destroy" />
+        </x-slot:actions>
+</x-modal>
 </div>
--- resources/views/livewire/admin/users/index.blade.php
+++ resources/views/livewire/admin/users/index.blade.php
@@ -74,7 +74,9 @@
     @can(App\Enum\Can::BE_AN_ADMIN->value)
         <x-button icon="o-pencil" wire:click="edit({{ $user->id }})" spinner class="btn-sm" />
     @unless ($user->trashed())
-        <x-button icon="o-trash" wire:click="delete({{ $user->id }})" spinner class="btn-sm" />
+            <livewire:admin.users.delete :user="$user" wire:key="delete-btn-{{$user->id}}" />
+
+        {{-- <x-button icon="o-trash" wire:click="delete({{ $user->id }})" spinner class="btn-sm" /> --}}
     @else
         <x-button icon="o-arrow-path-rounded-square" wire:click="delete({{ $user->id }})" spinner class="btn-sm btn-success btn-ghost" />
     @endunless
