f18cda27dd37fa13040c7207d3bbb0912ad0a9d1
Keny R C Noleto <kenyrcnoleto@gmail.com>
2026-03-13 13:21:54 -0300

    crm-14: frontend: deletion confirmation dialog

--- resources/views/livewire/admin/users/delete.blade.php
+++ resources/views/livewire/admin/users/delete.blade.php
@@ -2,7 +2,11 @@
     {{-- Do your work, then step back. --}}
     <x-button icon="o-trash" @click="$wire.modal = true" spinner class="btn-sm" />
 
-    <x-modal wire:model="modal" title="Deletion Confirmation" separator>
+    <x-modal
+        wire:model="modal"
+        title="Deletion Confirmation"
+        subtitle="You are deleting the user {{ $user->name }}"
+        separator>
 
         @error('confirmation')
             <x-alert icon="o-exclamation-triangle" class="alert-danger"  >
@@ -10,8 +14,11 @@
             </x-alert>
         @enderror
 
-        <x-input label="Write 'KENOBI' to confirm the deletion of the user {{ $user->name }}"
-             wire:model="confirmation_confirmation" />
+        <x-input
+            class="input-sm"
+            label="Write 'KENOBI' to confirm the deletion"
+             wire:model="confirmation_confirmation"
+        />
 
          {{-- We can use the `actions` slot from `x-modal` to place the buttons in the right place, but we can also place them outside of it, as long as we use the `actions` slot from `x-form` inside the modal. --}}
          {{-- <x-slot:actions></x-slot:actions>
