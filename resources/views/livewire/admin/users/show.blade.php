f0a4eaddace0d135c5e8cab2f8279b869f040e1e
Keny R C Noleto <kenyrcnoleto@gmail.com>
2026-03-31 12:25:37 -0300

    crm-13: displaying the information inside a drawer instead of a modal

--- resources/views/livewire/admin/users/show.blade.php
+++ resources/views/livewire/admin/users/show.blade.php
@@ -1,13 +1,12 @@
 <div>
     {{-- The Master doesn't talk, he acts. --}}
 
-
-    <x-modal wire:model="modal" :title="$user?->name" class="backdrop-blur">
+    <x-drawer wire:model="modal" :title="$user?->name" class="w-11/12 p-4 backdrop-blur" right>
         @if ($user)
             <div class="space-y-2">
 
-                <x-input readonly label="Name" value:model="$user->name" />
-                <x-input readonly label="Email" value:model="$user->email" />
+                <x-input readonly label="Name" :value="$user->name" />
+                <x-input readonly label="Email" :value="$user->email" />
                 <x-input readonly label="Created At" :value="$user->created_at->format('d/m/Y H:i')" />
                 <x-input readonly label="Updated At" :value="$user->updated_at->format('d/m/Y H:i')" />
                 <x-input readonly label="Deleted At" :value="$user->deleted_at?->format('d/m/Y H:i') ?? 'N/A'" />
@@ -18,7 +17,7 @@
         <x-slot:actions>
             <x-button label="Cancel" @click="$wire.modal = false" />
         </x-slot:actions>
-    </x-modal>
+    </x-drawer>
 
     <x-button label="Open" @click="$wire.modal = true" />
 </div>
