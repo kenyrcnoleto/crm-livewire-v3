5dab6ad87c9e8bf66ae4adf4a25e4e96fd558fbd
Keny R C Noleto <kenyrcnoleto@gmail.com>
2026-04-10 11:43:13 -0300

    crm-7: making sure we are loggin with the give user

--- tests/Feature/Admin/UserMagement/ImpersonaeTest.php
+++ tests/Feature/Admin/UserMagement/ImpersonaeTest.php
@@ -4,6 +4,7 @@ use App\Livewire\Admin\Users\Impersonate;
 use App\Models\User;
 use Livewire\Livewire;
 
+use function Pest\Laravel\{actingAs, get};
 use function PHPUnit\Framework\{assertSame, assertTrue};
 
 test('it should add a key impersonate to the session with the given user', function () {
@@ -16,4 +17,21 @@ test('it should add a key impersonate to the session with the given user', funct
     assertTrue(session()->has('impersonate'));
 
     assertSame(session()->get('impersonate'), $user->id);
-})->only();
+});
+
+test('should make sure that we are logged with the impersonate user', function () {
+    $admin = User::factory()->admin()->create();
+    $user  = User::factory()->create();
+
+    actingAs($admin);
+
+    expect(auth()->id())->toBe($admin->id);
+
+    Livewire::test(Impersonate::class)
+        ->call('impersonate', $user->id);
+
+    get(route('dashboard'))
+        ->assertSee(__("You're impersonating :name, click here to stop impersonating.", ['name' => $user->name]));
+
+    expect(auth()->id())->toBe($user->id);
+});
